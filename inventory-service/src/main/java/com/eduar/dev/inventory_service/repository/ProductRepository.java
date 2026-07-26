0087f035793d7d590bb45199fdfa91ac71a98bf7
EduarRugamas <juaneduardo021299@hotmail.com>
2026-07-25 20:59:05 -0600

    Eliminacion de metodo en service product no usado

--- inventory-service/src/main/java/com/eduar/dev/inventory_service/repository/ProductRepository.java
+++ inventory-service/src/main/java/com/eduar/dev/inventory_service/repository/ProductRepository.java
@@ -1,8 +1,5 @@
 package com.eduar.dev.inventory_service.repository;
 
-
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,14 +13,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsBySkuIgnoreCase(String sku);
 
     Page<Product> findByCategory(ProductCategory category, Pageable page);
-
-    // @Query("""
-    //         SELECT product
-    //         FROM Product product
-    //         WHERE product.currentStock <= product.minStock
-    //         ORDER BY product.currentStock ASC
-    //         """)
-    // List<Product> findProductsWithLowStock();
 
     @Query("""
             SELECT COUNT(product)
