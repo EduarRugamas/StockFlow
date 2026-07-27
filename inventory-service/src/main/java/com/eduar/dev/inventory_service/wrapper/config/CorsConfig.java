db7227395f44d1c3338af35713309d1675c92a88
EduarRugamas <juaneduardo021299@hotmail.com>
2026-07-26 23:45:49 -0600

    CORS: Configuracion de CorsConfig

---
+++ inventory-service/src/main/java/com/eduar/dev/inventory_service/wrapper/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.eduar.dev.inventory_service.wrapper.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "PATCH",
+                        "DELETE",
+                        "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .exposedHeaders("Location")
+                .allowCredentials(true)
+                .maxAge(3600);
+        }
+
+
+}
